a25144770b2c738bd63ed373c19c8f20bd9c950d
Juhász Péter <peter.juhasz@live.com>
2026-09-24 19:07:33 +0200

    buffer writer write

---
+++ src/PeterJuhasz.Extensions/Serialization/BufferWriterExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace System.Buffers;
+
+public static partial class Extensions
+{
+	public static void Write<TWriter>(this TWriter writer, byte value) where TWriter : IBufferWriter<byte>
+	{
+		var span = writer.GetSpan(1);
+		span[0] = value;
+		writer.Advance(1);
+	}
+
+	public static void Write<TWriter>(this TWriter writer, char value) where TWriter : IBufferWriter<char>
+	{
+		var span = writer.GetSpan(1);
+		span[0] = value;
+		writer.Advance(1);
+	}
+}
